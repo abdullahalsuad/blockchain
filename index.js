8c6a2458abd25b442bec7e6fa64685dbaa6c8ea6
suad <suadabdullahal@gmail.com>
2022-02-04 01:14:19 +0600

    2nd commit

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
+const sha256 = require('crypto-js/sha256');
+
 class Block{
     constructor(timestamp,data,previousHash){
         this.timestamp = timestamp;
         this.data = data;
         this.previousHash = previousHash;
+        this.hash = this.calculateHash();
     }
 
     calculateHash(){
-        
+         return sha256( this.timestamp + JSON.stringify(this.data) + this.previousHash ).toString();   
     }
 }
 
 const block = new Block("2020-01-01",{amount: 5},"NFT");
+console.log(block);
